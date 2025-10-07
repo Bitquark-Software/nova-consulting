f99574909b735abba2a935e8e305842c8359b8e5
Fernando Morales <idsfernandomorales@gmail.com>
2025-10-07 09:25:23 -0600

    Gtag added

--- resources/views/layouts/global_header.blade.php
+++ resources/views/layouts/global_header.blade.php
@@ -25,6 +25,16 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BSEMW3GN2Y"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BSEMW3GN2Y');
+    </script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Seo tags -->
